d4c66e666b57b865507f470fb6f9f6aa41aff350
Antoni <antoni.protoni@gmail.com>
2022-02-01 23:58:20 -0300

    funcionalidad menu , nueva secccion

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-alert("hi there")
+
+
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+// console.log(toggle)
+
+for (const element of toggle) {
+  element.addEventListener('click', menuClick)
+
+}
+
+function menuClick() {
+  nav.classList.toggle('show')
+}
+
+
+const menuList = document.querySelectorAll(' .menu ul li a')
+
+for (const element of menuList) {
+  element.addEventListener('click', irAseccion)
+}
+console.log(menuList)
+
+function irAseccion() {
+  nav.classList.remove('show')
+}
+
