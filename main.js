8aebdb6d36be21ec3d677f3cadf0348dbfe86d7c
Antoni <antoni.protoni@gmail.com>
2022-02-03 11:56:56 -0300

    finalizada seccion testimonios y seccion contacto

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 
 
-
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 // console.log(toggle)
@@ -53,6 +52,40 @@ window.addEventListener('scroll',
 // })
 
 
+// SWIPER pagination , slides, carrusell 
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
 
 
 
+// Scrollreveal.org efecto mostrar elementos cuando se hace scroll en la pagina
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image , #home .text ,
+  #about .image, #about .text,
+  #services header , #services .card,
+  #testimonials header , #testimonials .testimonials ,
+  #contact .text , #contact .links
+  `
+
+
+  , { interval: 100 })
